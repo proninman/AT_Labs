8e19c0b5725d91614ef2015427165f1ba1c54d09
proninman <proninman@gmail.com>
2021-04-26 13:18:38 +0300

    Комми 1 Передача на удаленный репозиторий тестовой программы из Вижуал студио

---
+++ AT_calc2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AT_calc2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
